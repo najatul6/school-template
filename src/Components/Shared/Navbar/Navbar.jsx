5495c0193ba00a7e9571ca57790fb26d86253df3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-12 05:00:39 +0600

    Refactor Navbar component: remove trailing space in active NavLink class for cleaner styling

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                 to={route?.path}
                 className={({ isActive }) =>
                   `px-2 py-3 block ${
-                    isActive ? "text-primary-color " : ""
+                    isActive ? "text-primary-color" : ""
                   } hover:text-primary-color`
                 }
               >
