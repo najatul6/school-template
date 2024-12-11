c4a68ac86c2877c27bfd9e7dd52f300bef114d1f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-11 22:09:57 +0600

    Add routes for Home and Contact components in Router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,21 @@
 import { createBrowserRouter } from "react-router-dom"
 import Layout from "../Layout/Layout"
+import Home from "../Pages/Home/Home"
+import Contact from "../Pages/Contact/Contact"
 
 const Router = createBrowserRouter([
     {
         path: '/',
         element: <Layout/>,
         children: [
-            {}
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/contact',
+                element: <Contact/>
+            }
         ]
     }
 ])
