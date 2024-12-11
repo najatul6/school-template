2aebebb99f64ccb807c52800f7544f4bd7f539c0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-12 03:56:06 +0600

    Refactor Button component to support background options and improve styling

--- src/Components/Shared/Button/Button.jsx
+++ src/Components/Shared/Button/Button.jsx
@@ -1,26 +1,38 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
-const Button = ({text,icon,className}) => {
+const Button = ({ text, icon, withBg, withOutBg, whiteText }) => {
   return (
-    <button className={`${className} text-primary-color hover:text-light-color flex justify-center items-center gap-2 px-5 py-3 rounded-md text-xl font-bold`}>
-     {text} {icon}
+    <button
+      className={`${
+        withBg
+          ? "bg-primary-color text-light-color hover:bg-transparent border border-primary-color"
+          : withOutBg
+          ? "bg-transparent border border-primary-color text-primary-color hover:bg-primary-color hover:text-light-color"
+          : whiteText
+          ? "text-light-color hover:text-primary-color"
+          : "text-primary-color hover:text-light-color"
+      }  flex justify-center items-center gap-2 px-5 capitalize py-3 rounded-md text-xl font-bold`}
+    >
+      {text} {icon}
     </button>
-  )
-}
+  );
+};
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
   icon: PropTypes.element,
-  className: PropTypes.string
-}
+  withBg: PropTypes.bool,
+  withOutBg: PropTypes.bool,
+  whiteText: PropTypes.bool,
+};
 
-export default Button
+export default Button;
 
 // with bg
 // bg-primary-color text-light-color hover:bg-transparent border border-primary-color
 
-// without bg 
+// without bg
 // bg-transparent border border-primary-color text-primary-color hover:bg-primary-color hover:text-light-color
 
-// white text 
+// white text
 // text-light-color hover:text-primary-color
