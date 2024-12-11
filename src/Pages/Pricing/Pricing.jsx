d0a79cb7f993d59baa5c2663d1405411b8f1379a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-11 22:43:18 +0600

    Add Pricing and Product pages with placeholder content

---
+++ src/Pages/Pricing/Pricing.jsx
@@ -0,0 +1,9 @@
+const Pricing = () => {
+  return (
+    <div className="min-h-screen  flex justify-center items-center text-center text-3xl font-bold">
+      Pricing Page coming Soon...
+    </div>
+  );
+};
+
+export default Pricing;
---
+++ src/Pages/Product/Product.jsx
@@ -0,0 +1,9 @@
+const Product = () => {
+  return (
+    <div className="min-h-screen  flex justify-center items-center text-center text-3xl font-bold">
+      Product Page coming Soon...
+    </div>
+  );
+};
+
+export default Product;
