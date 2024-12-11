84089b16887e7ce86788be83f57d93ff4a335dfb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-11 22:52:48 +0600

    Add Montserrat font to body and update Tailwind configuration for Jost font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
       },
       fontFamily:{
         "montserrat":["Montserrat", "sans-serif"],
-        "poppins":["Montserrat", "sans-serif"],
+        "jost":["Jost", "serif"],
       }
     },
   },
