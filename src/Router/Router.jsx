ea379146212aa5da42de184be82cddcd0bccc29f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-11 22:53:36 +0600

    Add Product and Pricing routes to the router configuration

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,23 +1,33 @@
-import { createBrowserRouter } from "react-router-dom"
-import Layout from "../Layout/Layout"
-import Home from "../Pages/Home/Home"
-import Contact from "../Pages/Contact/Contact"
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../Pages/Home/Home";
+import Contact from "../Pages/Contact/Contact";
+import Product from "../Pages/Product/Product";
+import Pricing from "../Pages/Pricing/Pricing";
 
 const Router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout/>,
-        children: [
-            {
-                path: '/',
-                element: <Home/>
-            },
-            {
-                path: '/contact',
-                element: <Contact/>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product",
+        element: <Product />,
+      },
+      {
+        path: "/pricing",
+        element: <Pricing />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
-export default Router
+export default Router;
