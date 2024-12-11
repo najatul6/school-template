6ef317793b0566294808163e820f964843461305
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-12 04:44:08 +0600

    Enhance Navbar component: add LogIn and JOIN US buttons with icons, improve menu transition effects, and update styling for better responsiveness

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { IoClose, IoMenu } from "react-icons/io5";
 import { menu } from "../../../Utils/routes";
 import Button from "../Button/Button";
+import { FaArrowRight } from "react-icons/fa";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,26 +26,34 @@ const Navbar = () => {
           isOpen ? "h-[17 rem]" : " h-0 overflow-hidden "
         } top-full  absolute left-0 w-full z-[100] bg-gradient-back backdrop-blur-2xl shadow-2xl shadow-gradient-back transition-all duration-500 lg:h-auto lg:static lg:w-auto lg:bg-inherit lg:backdrop-blur-0 lg:shadow-none delay-700 lg:delay-0`}
       >
-        <ul className="lg:flex gap-6">
+        <ul className="lg:flex lg:gap-6">
           {menu?.map((route) => (
             <li
               key={route?.id}
               className={`${
                 isOpen
                   ? "translate-y-0 transition-all duration-300"
-                  : "translate-y-[-50px] transition-all duration-700"
+                  : "translate-y-[-50px] transition-all duration-500"
               } text-white font-medium text-lg lg:text-xl my-[1.5rem] lg:my-0 mx-0 lg:ml-10 block text-center duration-500 transition lg:translate-y-0 `}
             >
-              <NavLink to={route?.path} className=" px-2 py-3 block">
+              <NavLink
+                onClick={() => setIsOpen(!isOpen)}
+                to={route?.path}
+                className=" px-2 py-3 block"
+              >
                 {route?.name}
               </NavLink>
             </li>
           ))}
+          <div className="lg:hidden flex justify-center items-center flex-col pb-5">
+            <Button text="LogIn" whiteText />
+            <Button text="JOIN US" icon={<FaArrowRight />} withBg />
+          </div>
         </ul>
       </nav>
       <div className="lg:flex hidden">
-        <Button text="LogIn" whiteText/>
-        <Button text="JOIN US" withBg/>
+        <Button text="LogIn" whiteText />
+        <Button text="JOIN US" withBg icon={<FaArrowRight />} />
       </div>
       {/* <div className="dropdown dropdown-end">
         <div tabIndex={0} role="button" className=" m-1">
