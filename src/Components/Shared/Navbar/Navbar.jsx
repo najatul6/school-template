2255254875fed90a9ab27e9893a9e9d8b44a0d18
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-11 22:56:17 +0600

    Update Navbar component background color to dark theme

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-dark-background-color">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
