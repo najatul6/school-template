3f4d968b4d293e0424f191b28a035e2fda34733a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-12 07:36:57 +0600

    Refactor Banner component: enhance layout and styling for better responsiveness and visual appeal

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,11 +5,11 @@ import Container from "../Shared/Container/Container";
 const Banner = () => {
   return (
     <Container>
-      <div className="flex justify-center items-center gap-[30px] text-light-color">
-        <div className="flex flex-col justify-start items-start ">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-[30px] text-light-color py-5">
+        <div className="flex flex-col justify-start items-start gap-[30px] lg:w-1/3">
           <h6 className="font-bold tracking-[0.1px] text-primary-color">Welcome</h6>
           <h1 className="font-bold text-4xl">Best Learning Opportunities</h1>
-          <p>Every day brings with it a fresh set of learning possibilities.</p>
+          <p className="text-xl tracking-[0.2px]">Every day brings with it a fresh set of learning possibilities.</p>
           <div className="flex justify-start items-center gap-[10px]">
             <Button text={"Get Quote Now"} withBg />
             <Button text={"Learn More"} withOutBg />
