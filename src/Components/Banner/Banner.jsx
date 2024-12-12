d082e328b39c53d16e32e186475efe4130dcc560
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-12 07:31:48 +0600

    Add Container wrapper to Banner component for improved layout and styling

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,23 @@
 import Button from "../Shared/Button/Button";
 import banner from "../../assets/none.png";
+import Container from "../Shared/Container/Container";
 
 const Banner = () => {
   return (
-    <div className="flex justify-center items-center gap-[30px]">
-      <div className="flex flex-col justify-start items-start ">
-        <h6>Welcome</h6>
-        <h1>Best Learning Opportunities</h1>
-        <p>Every day brings with it a fresh set of learning possibilities.</p>
-        <div className="flex justify-start items-center gap-[10px]">
-          <Button text={"Get Quote Now"} withBg />
-          <Button text={"Learn More"} withOutBg />
+    <Container>
+      <div className="flex justify-center items-center gap-[30px] text-light-color">
+        <div className="flex flex-col justify-start items-start ">
+          <h6 className="font-bold tracking-[0.1px] text-primary-color">Welcome</h6>
+          <h1 className="font-bold text-4xl">Best Learning Opportunities</h1>
+          <p>Every day brings with it a fresh set of learning possibilities.</p>
+          <div className="flex justify-start items-center gap-[10px]">
+            <Button text={"Get Quote Now"} withBg />
+            <Button text={"Learn More"} withOutBg />
+          </div>
         </div>
+        <img src={banner} alt="Student Banner image" />
       </div>
-      <img src={banner} alt="Student Banner image" />
-    </div>
+    </Container>
   );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Banner from "../../Components/Banner/Banner"
 
 const Home = () => {
   return (
-    <div className="min-h-screen  flex justify-center items-center text-center text-3xl font-bold">
+    <div className="">
       <Banner/>
     </div>
   )
