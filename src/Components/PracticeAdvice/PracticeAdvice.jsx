f64520b58450bbaa4285c779613f17645c468a9a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-13 21:01:43 +0600

    Update PracticeAdvice component: integrate SectionHeader with title, subtitle, and description

--- src/Components/PracticeAdvice/PracticeAdvice.jsx
+++ src/Components/PracticeAdvice/PracticeAdvice.jsx
@@ -1,9 +1,23 @@
-import Container from "../Shared/Container/Container"
+import Container from "../Shared/Container/Container";
+import SectionHeader from "../Shared/SectionHeader/SectionHeader";
 
 const PracticeAdvice = () => {
   return (
-    <Container>PracticeAdvice</Container>
-  )
-}
+    <Container>
+      <div className="w-1/2">
+        <SectionHeader
+          subTitle={"Practice Advice"}
+          title={"Every Client Matters"}
+          description={
+            "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics "
+          }
+        />
+      </div>
+<div>
+    
+</div>
+    </Container>
+  );
+};
 
-export default PracticeAdvice
+export default PracticeAdvice;
