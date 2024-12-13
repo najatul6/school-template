4dcb2e1bf7cd1635c329f843064b1cb552156326
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-13 19:55:23 +0600

    Update Tailwind configuration: import daisyui directly in plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -15,5 +16,5 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
