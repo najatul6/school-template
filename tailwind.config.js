4059538f9324dbdfb82f2dee9a2d73a7da67e1aa
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-13 19:51:57 +0600

    Update Tailwind configuration: streamline color and font family definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        "danger-color":"#E74040",
-        "dark-background-color":"#26335D",
-        "light-color":"#FFFFFF",
-        "primary-color":"#8D5CF6"
+      colors: {
+        "danger-color": "#E74040",
+        "dark-background-color": "#26335D",
+        "light-color": "#FFFFFF",
+        "primary-color": "#8D5CF6",
       },
-      fontFamily:{
-        "montserrat":["Montserrat", "sans-serif"],
-        "jost":["Jost", "serif"],
-      }
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        jost: ["Jost", "serif"],
+      },
     },
   },
-  plugins: [
-    require('daisyui')
-  ]
+  plugins: [require("daisyui")],
 };
