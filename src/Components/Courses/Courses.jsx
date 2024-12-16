f3010aeddb8d6f2e06ba1911fa49533bef8236e3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-16 23:48:59 +0600

    Refactor Courses component: improve structure and formatting for better readability

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,15 +1,21 @@
-import Container from "../Shared/Container/Container"
-import SectionHeader from "../Shared/SectionHeader/SectionHeader"
+import Container from "../Shared/Container/Container";
+import SectionHeader from "../Shared/SectionHeader/SectionHeader";
 
 const Courses = () => {
   return (
     <Container>
-        <SectionHeader title={"Most Popular Courses"} subTitle={"Courses"} description={"Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics"}/>
-        <div>
-            
-        </div>
+      <div className="w-1/2">
+        <SectionHeader
+          title={"Most Popular Courses"}
+          subTitle={"Courses"}
+          description={
+            "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics"
+          }
+        />
+      </div>
+      <div></div>
     </Container>
-  )
-}
+  );
+};
 
-export default Courses
+export default Courses;
