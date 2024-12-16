3bedcf161adc54ff5979c5d434ac9361deb78b32
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-16 23:32:55 +0600

    Update Courses component: integrate Container and SectionHeader for enhanced layout

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,7 +1,11 @@
+import Container from "../Shared/Container/Container"
+import SectionHeader from "../Shared/SectionHeader/SectionHeader"
 
 const Courses = () => {
   return (
-    <div>Courses</div>
+    <Container>
+        <SectionHeader title={"Most Popular Courses"} subTitle={"Courses"} description={"Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics"}/>
+    </Container>
   )
 }
 
