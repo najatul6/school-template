646f2c1f072452f3c7e448a9e450ea1ce76a6f55
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-19 20:29:51 +0600

    Refactor ProductCard component to improve icon layout and structure

--- src/Components/Shared/ProductCard/ProductCard.jsx
+++ src/Components/Shared/ProductCard/ProductCard.jsx
@@ -1,20 +1,26 @@
-import { FaEye, FaRegHeart } from "react-icons/fa"
-import { IoCartOutline } from "react-icons/io5"
+import { FaEye, FaRegHeart } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
 
-const ProductCard = ({course}) => {
+const ProductCard = ({ course }) => {
   return (
     <div>
       <div>
         <img src="https://via.placeholder.com/150" alt="product" />
         <p>Sale</p>
         <div>
-          <FaRegHeart/> 
-          <IoCartOutline />
-          <FaEye />
+          <div>
+            <FaRegHeart />
+          </div>
+          <div>
+            <IoCartOutline />
+          </div>
+          <div>
+            <FaEye />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
