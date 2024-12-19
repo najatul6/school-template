ada3841d32df5ffed4e6d87472b3e9123b5fd1f1
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-19 20:28:02 +0600

    Enhance ProductCard component by adding sale label and icons for user interaction

--- src/Components/Shared/ProductCard/ProductCard.jsx
+++ src/Components/Shared/ProductCard/ProductCard.jsx
@@ -1,9 +1,16 @@
+import { FaRegHeart } from "react-icons/fa"
+import { IoCartOutline } from "react-icons/io5"
 
 const ProductCard = ({course}) => {
   return (
     <div>
       <div>
         <img src="https://via.placeholder.com/150" alt="product" />
+        <p>Sale</p>
+        <div>
+          <FaRegHeart/> 
+          <IoCartOutline />
+        </div>
       </div>
     </div>
   )
