27f2a19f6de122374d185397668f60b37ab85c4d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-19 20:57:28 +0600

    Add PropTypes validation to ProductCard component and improve styling

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -27,7 +27,7 @@ const Courses = () => {
           }
         />
       </div>
-      <div>
+      <div className="mt-[112px]">
         {loading ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             {Array(4)
--- src/Components/Shared/ProductCard/ProductCard.jsx
+++ src/Components/Shared/ProductCard/ProductCard.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { FaEye, FaRegHeart, FaStar } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 
@@ -14,18 +15,20 @@ const ProductCard = ({ course }) => {
   } = course;
   return (
     <div>
-      <div>
-        <img src={image} alt={name} />
-        <p>Sale</p>
-        <div>
-          <div>
-            <FaRegHeart />
+      <div className="relative overflow-hidden">
+        <img src={image} alt={name} className="w-full" />
+        <p className="absolute top-5 left-5 p-1 bg-danger-color font-bold rounded-md text-light-color">
+          Sale
+        </p>
+        <div className="absolute bottom-5 left-[25%] flex gap-2">
+          <div className="p-[10px] bg-light-color rounded-full text-dark-background-color cursor-pointer hover:text-primary-color hover:bg-dark-background-color duration-300">
+            <FaRegHeart className="text-xl" />
           </div>
-          <div>
-            <IoCartOutline />
+          <div className="p-[10px] bg-light-color rounded-full text-dark-background-color cursor-pointer hover:text-primary-color hover:bg-dark-background-color duration-300">
+            <IoCartOutline className="text-xl" />
           </div>
-          <div>
-            <FaEye />
+          <div className="p-[10px] bg-light-color rounded-full text-dark-background-color cursor-pointer hover:text-primary-color hover:bg-dark-background-color duration-300">
+            <FaEye className="text-xl" />
           </div>
         </div>
       </div>
@@ -46,6 +49,10 @@ const ProductCard = ({ course }) => {
       </div>
     </div>
   );
+};
+
+ProductCard.propTypes = {
+  course: PropTypes.object,
 };
 
 export default ProductCard;
