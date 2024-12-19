6dd332d76177fecdfe1bcb19de73545fd32a520f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-19 20:40:13 +0600

    Destructure course properties in ProductCard component for improved readability

--- src/Components/Shared/ProductCard/ProductCard.jsx
+++ src/Components/Shared/ProductCard/ProductCard.jsx
@@ -2,6 +2,7 @@ import { FaEye, FaRegHeart, FaStar } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 
 const ProductCard = ({ course }) => {
+  const { title, name, description, totalSell, price, discountPrice, rating, image} = course;
   return (
     <div>
       <div>
@@ -21,13 +22,18 @@ const ProductCard = ({ course }) => {
       </div>
       <div>
         <div>
-        <h4>{course?.title}</h4>
+          <h4>{course?.title}</h4>
+          <div>
+            <p>
+              <FaStar />
+            </p>
+            <p>{course?.rating}</p>
+          </div>
+        </div>
         <div>
-          <p><FaStar /></p>
-          <p>{course?.rating}</p>
+          <p>{course?.price}</p>
+          <p>{course?.discountPrice}</p>
         </div>
-        </div>
-        <p>{course?.price}</p>
       </div>
     </div>
   );
