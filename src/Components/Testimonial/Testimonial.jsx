d8a40faf2ddda3664e945c735d44a9e775232aa0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-20 19:32:49 +0600

    Fetch and display testimonials in Testimonial component with loading state

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -1,8 +1,20 @@
 import Marquee from "react-fast-marquee";
 import Container from "../Shared/Container/Container";
 import SectionHeader from "../Shared/SectionHeader/SectionHeader";
+import { useEffect, useState } from "react";
 
 const Testimonial = () => {
+  const [testimonials, setTestimonials] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/reviews.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setTestimonials(data);
+        setLoading(false);
+      });
+  }, []);
   return (
     <Container>
       <SectionHeader
@@ -13,7 +25,29 @@ const Testimonial = () => {
         }
       />
       <div>
-        <Marquee></Marquee>
+        <Marquee>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            testimonials.map((testimonial, index) => (
+              <div key={index} className="flex flex-col gap-5">
+                <div className="flex flex-col justify-center items-center gap-5 bg-light-color">
+                  <img
+                    src={testimonial.profilePic}
+                    alt={testimonial.name}
+                    className="w-[50px] h-[50px] rounded-full"
+                  />
+                    <p className="text-sm font-medium tracking-[0.2px]  opacity-[0.6]">
+                      {testimonial.description}
+                    </p>
+                </div>
+                <p className="text-sm font-medium tracking-[0.2px] text-dark-background-color">
+                  {testimonial.review}
+                </p>
+              </div>
+            ))
+          )}
+        </Marquee>
       </div>
     </Container>
   );
