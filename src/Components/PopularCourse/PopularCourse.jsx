8351bb6697be1e3445dd18062c1ce9b87e2475d9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-20 19:11:26 +0600

    Refactor PopularCourse component layout for improved responsiveness and visual alignment

--- src/Components/PopularCourse/PopularCourse.jsx
+++ src/Components/PopularCourse/PopularCourse.jsx
@@ -1,20 +1,28 @@
-import Container from "../Shared/Container/Container"
-import image from "../../assets/thumb-concept.png"
-import SectionHeader from "../Shared/SectionHeader/SectionHeader"
-import Button from "../Shared/Button/Button"
-import { MdKeyboardArrowRight } from "react-icons/md"
+import Container from "../Shared/Container/Container";
+import image from "../../assets/thumb-concept.png";
+import SectionHeader from "../Shared/SectionHeader/SectionHeader";
+import Button from "../Shared/Button/Button";
+import { MdKeyboardArrowRight } from "react-icons/md";
 const PopularCourse = () => {
   return (
     <Container>
-        <div>
-            <img src={image} alt="Child with smile face" />
-            <div>
-                <SectionHeader title={"Our Popular Courses"} subTitle={"Popular Courses"} description={"Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics "}/>
-                <Button text="Learn More" icon={<MdKeyboardArrowRight />} whiteText/>
-            </div>
+      <div className="flex justify-center items-center gap-10">
+        <div className="w-1/2">
+          <img src={image} alt="Child with smile face" className="w-full" />
         </div>
+        <div className="w-1/3">
+          <SectionHeader
+            title={"Our Popular Courses"}
+            subTitle={"Popular Courses"}
+            description={
+              "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics "
+            }
+          />
+          <Button text="Learn More" icon={<MdKeyboardArrowRight />} whiteText />
+        </div>
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default PopularCourse
+export default PopularCourse;
