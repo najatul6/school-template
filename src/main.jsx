a4e7b5b3de33f20db15ee61db044786b9240906f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:26:33 +0600

    Format code in main.jsx for improved readability and consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import Router from './Router/Router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Router/Router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <RouterProvider router={Router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={Router} />
+  </StrictMode>
+);
