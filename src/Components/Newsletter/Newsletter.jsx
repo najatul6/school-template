8f109e085538d07ab03f91725455cbff3f5a4ce7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:18:46 +0600

    Update Newsletter component to replace SectionHeader with custom headings for improved content presentation

--- src/Components/Newsletter/Newsletter.jsx
+++ src/Components/Newsletter/Newsletter.jsx
@@ -1,11 +1,19 @@
 import Container from "../Shared/Container/Container";
-import SectionHeader from "../Shared/SectionHeader/SectionHeader";
 
 const Newsletter = () => {
   return (
     <Container>
-      <div>
-        <SectionHeader description={""} subTitle={""} title={""} />
+      <div className="text-white flex gap-6 flex-col justify-center items-start">
+        <h6 className="text-primary-color text-sm font-bold tracking-[0.2px]">
+          Newsletter
+        </h6>
+        <h1 className="text-[40px] font-bold capitalize">
+          Our Popular Courses
+        </h1>
+        <p className="text-sm tracking-[0.2px] capitalize">
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics: Newtonian mechanics
+        </p>
       </div>
     </Container>
   );
