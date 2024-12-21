926eaed70961f57385ab80f9b58b276b3b275798
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:24:28 +0600

    Refactor Contact component for improved code formatting and readability

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,9 @@
-
 const Contact = () => {
   return (
     <div className="min-h-screen  flex justify-center items-center text-center text-3xl font-bold">
       Contact Page coming Soon...
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
