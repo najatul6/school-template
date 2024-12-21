dd1506968695ac9931470510271939713cc4d0cd
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:10:15 +0600

    Fix TeamCard component to handle optional member data and update social media links

--- src/Components/Team/TeamCard.jsx
+++ src/Components/Team/TeamCard.jsx
@@ -3,28 +3,37 @@ import { Link } from "react-router-dom";
 
 const TeamCard = ({ member }) => {
   return (
-    <div key={member._id} className="bg-white rounded-lg">
+    <div  className="bg-white rounded-lg">
       <div className="h-[230px]  rounded-t-lg">
         <img
-          src={member.image}
-          alt={member.name}
+          src={member?.image}
+          alt={member?.name}
           className="w-full h-full rounded-t-lg"
         />
       </div>
       <div className="mt-4">
         <h4 className="text-dark-background-color text-center font-bold">
-          {member.name}
+          {member?.name}
         </h4>
         <p className="text-xs text-[#737373] text-center">{member.position}</p>
         <div>
-          <Link>
-            <FaFacebook className="text-blue-500" />
+          <Link to={member?.facebook}>
+            <FaFacebook
+              size={24}
+              className="text-primary-color hover:text-blue-500"
+            />
           </Link>
-          <Link>
-            <FaInstagram className="text-blue-400" />
+          <Link to={member?.instagram}>
+            <FaInstagram
+              size={24}
+              className="text-primary-color hover:text-blue-500"
+            />
           </Link>
-          <Link>
-            <FaTwitter className="text-blue-500" />
+          <Link to={member?.twitter}>
+            <FaTwitter
+              size={24}
+              className="text-primary-color hover:text-blue-500"
+            />
           </Link>
         </div>
       </div>
