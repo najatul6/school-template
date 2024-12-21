7e4827ae2b88ea2818914ff47f1bf4138ac9a6a3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 21:44:42 +0600

    Update SectionHeader in Team component with new title and description for enhanced context

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,12 +1,18 @@
-import Container from "../Shared/Container/Container"
-import SectionHeader from "../Shared/SectionHeader/SectionHeader"
+import Container from "../Shared/Container/Container";
+import SectionHeader from "../Shared/SectionHeader/SectionHeader";
 
 const Team = () => {
   return (
     <Container>
-      <SectionHeader title={""} subTitle={"Team"} description={""}/>
+      <SectionHeader
+        title={"Watch our Courses"}
+        subTitle={"Team"}
+        description={
+          "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics "
+        }
+      />
     </Container>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
