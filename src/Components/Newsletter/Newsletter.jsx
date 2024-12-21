a6cb34c56efb5752fc49bfae3dafc372b4c87f3b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:21:48 +0600

    Enhance Newsletter component with subscription input and button for user engagement

--- src/Components/Newsletter/Newsletter.jsx
+++ src/Components/Newsletter/Newsletter.jsx
@@ -3,7 +3,7 @@ import Container from "../Shared/Container/Container";
 const Newsletter = () => {
   return (
     <Container>
-      <div className="text-white flex gap-6 flex-col justify-center items-start">
+      <div className="text-white text-center flex gap-6 flex-col justify-center items-center">
         <h6 className="text-primary-color text-sm font-bold tracking-[0.2px]">
           Newsletter
         </h6>
@@ -14,6 +14,15 @@ const Newsletter = () => {
           Problems trying to resolve the conflict between the two major realms
           of Classical physics: Newtonian mechanics
         </p>
+      </div>
+      <div className="flex justify-center items-center mt-10">
+        <div className="join">
+          <input
+            className="input input-bordered join-item text-white placeholder:text-white focus:outline-none"
+            placeholder="Email"
+          />
+          <button className="btn bg-primary-color text-white hover:bg-dark-background-color join-item rounded-r-full">Subscribe</button>
+        </div>
       </div>
     </Container>
   );
