58a8a72d06f459821a352c0fd16204fe9aec1147
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:24:52 +0600

    Fix formatting in routes.js for improved readability

--- src/Utils/routes.js
+++ src/Utils/routes.js
@@ -1,6 +1,6 @@
 export const menu = [
-    { id: 1, path: "/", name: "Home", component: "Home" },
-    { id: 2, path: "/product", name: "Product", component: "Product" },
-    { id: 3, path: "/pricing", name: "Pricing", component: "Pricing" },
-    { id: 4, path: "/contact", name: "Contact", component: "Contact" },
-  ];
+  { id: 1, path: "/", name: "Home", component: "Home" },
+  { id: 2, path: "/product", name: "Product", component: "Product" },
+  { id: 3, path: "/pricing", name: "Pricing", component: "Pricing" },
+  { id: 4, path: "/contact", name: "Contact", component: "Contact" },
+];
