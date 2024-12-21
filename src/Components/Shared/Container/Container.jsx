a67600e6ed63e7a6063e8ecfad39809c3041d873
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:22:44 +0600

    Refactor Container component for improved code consistency and readability

--- src/Components/Shared/Container/Container.jsx
+++ src/Components/Shared/Container/Container.jsx
@@ -1,14 +1,10 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
-const Container = ({children}) => {
-  return (
-    <div className="max-w-[1440px] mx-auto px-4">
-        {children}
-    </div>
-  )
-}
+const Container = ({ children }) => {
+  return <div className="max-w-[1440px] mx-auto px-4">{children}</div>;
+};
 Container.propTypes = {
-  children: PropTypes.node.isRequired
-}
+  children: PropTypes.node.isRequired,
+};
 
-export default Container
+export default Container;
