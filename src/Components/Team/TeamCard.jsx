18eab27a4d149111d43a92d605e4ac69eeb53e44
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:02:42 +0600

    Enhance TeamCard component to accept member data and display member details

--- src/Components/Team/TeamCard.jsx
+++ src/Components/Team/TeamCard.jsx
@@ -1,8 +1,19 @@
-
-const TeamCard = () => {
+const TeamCard = ({ member }) => {
   return (
-    <div>TeamCard</div>
-  )
-}
+    <div key={member._id} className="bg-white rounded-lg">
+      <div className="h-[230px]  rounded-t-lg">
+        <img
+          src={member.image}
+          alt={member.name}
+          className="w-full h-full rounded-t-lg"
+        />
+      </div>
+      <div className="mt-4">
+        <h4 className="text-lg font-semibold">{member.name}</h4>
+        <p className="text-sm text-gray-500">{member.position}</p>
+      </div>
+    </div>
+  );
+};
 
-export default TeamCard
+export default TeamCard;
