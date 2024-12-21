09e13e943790a5802bbffb8123f37401b6c4891d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 21:43:19 +0600

    Add Team component to Home page and update imports for improved structure

---
+++ src/Components/Team/Team.jsx
@@ -0,0 +1,9 @@
+import Container from "../Shared/Container/Container"
+
+const Team = () => {
+  return (
+    <Container>Team</Container>
+  )
+}
+
+export default Team
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,25 @@
-import Banner from "../../Components/Banner/Banner"
-import Courses from "../../Components/Courses/Courses"
-import ExpertBanner from "../../Components/ExpertBanner/ExpertBanner"
-import PopularCourse from "../../Components/PopularCourse/PopularCourse"
-import PracticeAdvice from "../../Components/PracticeAdvice/PracticeAdvice"
-import Stats from "../../Components/Stats/Stats"
-import Testimonial from "../../Components/Testimonial/Testimonial"
+import Banner from "../../Components/Banner/Banner";
+import Courses from "../../Components/Courses/Courses";
+import ExpertBanner from "../../Components/ExpertBanner/ExpertBanner";
+import PopularCourse from "../../Components/PopularCourse/PopularCourse";
+import PracticeAdvice from "../../Components/PracticeAdvice/PracticeAdvice";
+import Stats from "../../Components/Stats/Stats";
+import Team from "../../Components/Team/Team";
+import Testimonial from "../../Components/Testimonial/Testimonial";
 
 const Home = () => {
   return (
     <div className="space-y-[120px]">
-      <Banner/>
-      <Stats/>
-      <ExpertBanner/>
-      <PracticeAdvice/>
-      <Courses/>
-      <PopularCourse/>
-      <Testimonial/>
+      <Banner />
+      <Stats />
+      <ExpertBanner />
+      <PracticeAdvice />
+      <Courses />
+      <PopularCourse />
+      <Testimonial />
+      <Team />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
