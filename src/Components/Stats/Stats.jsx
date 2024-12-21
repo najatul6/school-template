e37e790baa333e0cbc7f0a56226438524eaca080
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 22:23:40 +0600

    Refactor Stats component for improved code formatting and readability

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -6,22 +6,30 @@ const Stats = () => {
       <div className="w-full grid  grid-cols-2 md:grid-cols-4 gap-[30px]">
         <div className="stat place-items-center">
           <div className="stat-value text-primary-color">15K</div>
-          <div className="stat-desc text-light-color font-bold">Happy Customers</div>
+          <div className="stat-desc text-light-color font-bold">
+            Happy Customers
+          </div>
         </div>
 
         <div className="stat place-items-center">
           <div className="stat-value text-primary-color">150K</div>
-          <div className="stat-desc text-light-color font-bold">Monthly Visitors</div>
+          <div className="stat-desc text-light-color font-bold">
+            Monthly Visitors
+          </div>
         </div>
 
         <div className="stat place-items-center">
           <div className="stat-value text-primary-color">15</div>
-          <div className="stat-desc text-light-color font-bold">Countries Worldwide</div>
+          <div className="stat-desc text-light-color font-bold">
+            Countries Worldwide
+          </div>
         </div>
 
         <div className="stat place-items-center">
           <div className="stat-value text-primary-color">100+</div>
-          <div className="stat-desc text-light-color font-bold">Top Partners</div>
+          <div className="stat-desc text-light-color font-bold">
+            Top Partners
+          </div>
         </div>
       </div>
     </Container>
