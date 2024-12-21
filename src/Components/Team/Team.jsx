91c62944fbe302c167d7fe630e05bdbe902b1a3d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-21 21:46:39 +0600

    Wrap SectionHeader in a div for improved layout structure in Team component

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -4,13 +4,15 @@ import SectionHeader from "../Shared/SectionHeader/SectionHeader";
 const Team = () => {
   return (
     <Container>
-      <SectionHeader
-        title={"Watch our Courses"}
-        subTitle={"Team"}
-        description={
-          "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics "
-        }
-      />
+      <div>
+        <SectionHeader
+          title={"Watch our Courses"}
+          subTitle={"Team"}
+          description={
+            "Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics "
+          }
+        />
+      </div>
     </Container>
   );
 };
