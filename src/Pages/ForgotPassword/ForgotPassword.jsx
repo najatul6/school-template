2534466e836194f58620d70a673df70bc4631cfd
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-25 23:44:20 +0600

    Style updates for ForgotPassword component: improved layout, adjusted colors, and enhanced button styling

--- src/Pages/ForgotPassword/ForgotPassword.jsx
+++ src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -1,17 +1,17 @@
 
 const ForgotPassword = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md w-full">
-        <h1 className="text-center text-2xl font-bold mb-6">Forgot Password</h1>
+    <div className="min-h-screen flex items-center justify-center">
+    <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-lg w-full">
+        <h1 className="text-center text-white text-2xl font-bold mb-6">Forgot Password</h1>
         <form>
             <div className="mb-4">
-                <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+                <label className="block text-white font-bold mb-2" htmlFor="email">
                     Email Address
                 </label>
-                <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter your email address" />
+                <input className="appearance-none border rounded w-full py-2 px-3 text-primary-color leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter your email address" />
             </div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
+            <button className="bg-primary-color hover:bg-transparent border-2 duration-300 border-primary-color text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
                 Reset Password
             </button>
         </form>
