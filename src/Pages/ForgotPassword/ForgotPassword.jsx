1eaac2cacf664c47a9177db5bd53176bdab7a367
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-25 23:46:47 +0600

    Enhance styling of ForgotPassword component: update shadow effect, input background, and placeholder color for improved user experience

--- src/Pages/ForgotPassword/ForgotPassword.jsx
+++ src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -2,14 +2,14 @@
 const ForgotPassword = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
-    <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-lg w-full">
+    <div className="shadow-inner shadow-primary-color rounded px-8 pt-6 pb-8 mb-4 max-w-lg w-full">
         <h1 className="text-center text-white text-2xl font-bold mb-6">Forgot Password</h1>
         <form>
             <div className="mb-4">
                 <label className="block text-white font-bold mb-2" htmlFor="email">
                     Email Address
                 </label>
-                <input className="appearance-none border rounded w-full py-2 px-3 text-primary-color leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter your email address" />
+                <input className="appearance-none bg-transparent border border-primary-color rounded w-full py-2 px-3 text-primary-color leading-tight focus:outline-none focus:shadow-outline placeholder:text-primary-color" id="email" type="email" placeholder="Enter your email address" />
             </div>
             <button className="bg-primary-color hover:bg-transparent border-2 duration-300 border-primary-color text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
                 Reset Password
