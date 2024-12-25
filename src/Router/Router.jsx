93779352c892bd27743f153a7ef703119704f273
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-25 23:39:10 +0600

    Update routing to display ForgotPassword component for password recovery

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -6,6 +6,7 @@ import Product from "../Pages/Product/Product";
 import Pricing from "../Pages/Pricing/Pricing";
 import LogIn from "../Pages/LogIn/LogIn";
 import JoinUs from "../Pages/JoinUs/JoinUs";
+import ForgotPassword from "../Pages/ForgotPassword/ForgotPassword";
 
 const Router = createBrowserRouter([
   {
@@ -38,7 +39,7 @@ const Router = createBrowserRouter([
       },
       {
         path: "/forgotPassword",
-        element: <JoinUs />,
+        element: <ForgotPassword />,
       },
     ],
   },
