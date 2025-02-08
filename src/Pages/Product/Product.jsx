89c6f6d30e7e0bb144fccdb7654997e975ee9e7f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-08 23:56:55 +0600

    Update Product component: correct wording in the product page message

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,7 +1,7 @@
 const Product = () => {
   return (
     <div className="min-h-screen  flex justify-center items-center text-center text-3xl font-bold">
-      Product Page coming Soon...
+      Product Page is coming Soon...
     </div>
   );
 };
