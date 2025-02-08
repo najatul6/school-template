a94aa4b5713fabcb257c473c40ef0b5e5d1a9976
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-08 23:58:31 +0600

    Add Snapchat to social media array in routes utility

--- src/Utils/routes.js
+++ src/Utils/routes.js
@@ -6,41 +6,54 @@ export const menu = [
 ];
 
 
-export const socialMedia=[
-  {id:"202jd721038",
-    name:"Facebook",
-    link:"https://www.facebook.com/",
-    icon:"fab fa-facebook-f"
+export const socialMedia = [
+  {
+    id: "202jd721038",
+    name: "Facebook",
+    link: "https://www.facebook.com/",
+    icon: "fab fa-facebook-f"
 
   },
-  {id:"202jd721039",
-    name:"Instagram",
-    link:"https://www.instagram.com/",
-    icon:"fab fa-instagram"
+  {
+    id: "202jd721039",
+    name: "Instagram",
+    link: "https://www.instagram.com/",
+    icon: "fab fa-instagram"
 
   },
-  {id:"202jd721040",
-    name:"Twitter",
-    link:"https://www.twitter.com/",
-    icon:"fab fa-twitter"
+  {
+    id: "202jd721040",
+    name: "Twitter",
+    link: "https://www.twitter.com/",
+    icon: "fab fa-twitter"
 
   },
-  {id:"202jd721041",
-    name:"LinkedIn",
-    link:"https://www.linkedin.com/",
-    icon:"fab fa-linkedin"
+  {
+    id: "202jd721041",
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/",
+    icon: "fab fa-linkedin"
 
   },
-  {id:"202jd721042",
-    name:"YouTube",
-    link:"https://www.youtube.com/",
-    icon:"fab fa-youtube"
+  {
+    id: "202jd721042",
+    name: "YouTube",
+    link: "https://www.youtube.com/",
+    icon: "fab fa-youtube"
 
   },
-  {id:"202jd721043",
-    name:"Pinterest",
-    link:"https://www.pinterest.com/",
-    icon:"fab fa-pinterest"
+  {
+    id: "202jd721043",
+    name: "Pinterest",
+    link: "https://www.pinterest.com/",
+    icon: "fab fa-pinterest"
+
+  },
+  {
+    id: "202jd721044",
+    name: "Snapchat",
+    link: "https://www.snapchat.com/",
+    icon: "fab fa-snapchat"
 
   },
 ]
