b33578b36209e464c97ca510881fb804a59844ca
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-08 23:56:27 +0600

    Update Contact component: correct wording in the contact page message

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <div className="min-h-screen  flex justify-center items-center text-center text-3xl font-bold">
-      Contact Page coming Soon...
+      Contact Page is coming Soon...
     </div>
   );
 };
