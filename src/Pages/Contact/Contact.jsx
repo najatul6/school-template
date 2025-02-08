ceb94fa7c6b806f63aa256da0f6a4323cdad4419
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-08 23:59:57 +0600

    Remove unnecessary whitespace in Contact component

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <div className="min-h-screen  flex justify-center items-center text-center text-3xl font-bold">
+    <div className="min-h-screen flex justify-center items-center text-center text-3xl font-bold">
       Contact Page is coming Soon...
     </div>
   );
